6accad14019b384769870fc4c3cdd597f3c1f374
Cenk Berkay Taş <cenk@alastor.echidna.local>
2022-07-06 23:53:24 +0300

    add default behavior for submit button. log the string entered

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,15 @@
 console.log("heeeey");
 
+function validateForm() {
+    let x = document.forms["submit_task"]["task_name"].value;
+    if (x == "") {
+	alert("AAAAAAAAAAAAAAA");
+	return false;
+    }
+    else
+    {
+	console.log(x);
+	//create a task object with "done" bool and "string" name
+    }
+}
 
